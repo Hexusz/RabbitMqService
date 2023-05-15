0c28d22676d4999dd69396e503458d557977f5f7
Александр <alo0606@mail.ru>
2023-05-15 23:44:32 +0400

    Обработка сообщений из брокера

---
+++ RabbitMqListener/RabbitMq/RabbitMqListener.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace RabbitMqListener.RabbitMq
+{
+	public class RabbitMqListener : BackgroundService
+    {
+        private IConnection _connection;
+        private IModel _channel;
+
+        public RabbitMqListener()
+        {
+            var factory = new ConnectionFactory { Uri = new Uri("amqps://eiaearcf:sNsv_mLuPqGo8cAvQ4uQy2oy4vIZeZYk@toad.rmq.cloudamqp.com/eiaearcf") };
+            _connection = factory.CreateConnection();
+            _channel = _connection.CreateModel();
+            _channel.QueueDeclare(queue: "MyQueue", durable: false, exclusive: false, autoDelete: false, arguments: null);
+        }
+
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            stoppingToken.ThrowIfCancellationRequested();
+
+            var consumer = new EventingBasicConsumer(_channel);
+            consumer.Received += (ch, ea) =>
+            {
+                var content = Encoding.UTF8.GetString(ea.Body.ToArray());
+
+                // Обработка сообщения
+                Debug.WriteLine($"Обработка сообщение: {content}");
+
+                _channel.BasicAck(ea.DeliveryTag, false);
+            };
+
+            _channel.BasicConsume("MyQueue", false, consumer);
+
+            return Task.CompletedTask;
+        }
+
+        public override void Dispose()
+        {
+            _channel.Close();
+            _connection.Close();
+            base.Dispose();
+        }
+    }
+}
